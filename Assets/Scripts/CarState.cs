353d1ad6d578563d3aa796bc3f47e6affbfc8f62
Michal Bereznicki <michal.bereznicki@external.toyota-europe.com>
2025-05-12 18:00:38 +0200

    Car state and sensors

---
+++ Assets/Scripts/CarState.cs
@@ -0,0 +1,10 @@
+
+public class CarState
+{
+    public float Speed { get; set; }
+    public float LeftDistance { get; set; }
+    public float LeftFrontDistance { get; set; }
+    public float FrontDistance { get; set; }
+    public float RightFrontDistance { get; set; }
+    public float RightDistance { get; set; }
+}
---
+++ Assets/Scripts/CarStateProvider.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CarStateProvider : MonoBehaviour
+{
+    [SerializeField] Transform leftSensor;
+    [SerializeField] Transform leftFrontSensor;
+    [SerializeField] Transform frontSensor;
+    [SerializeField] Transform rightFrontSensor;
+    [SerializeField] Transform rightSensor;
+
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public CarState GetCarState()
+    {
+        return new CarState
+        {
+            Speed = rb.linearVelocity.magnitude,
+            LeftDistance = GetSensorDistance(leftSensor),
+            LeftFrontDistance = GetSensorDistance(leftFrontSensor),
+            FrontDistance = GetSensorDistance(frontSensor),
+            RightFrontDistance = GetSensorDistance(rightFrontSensor),
+            RightDistance = GetSensorDistance(rightSensor)
+        };
+    }
+
+    float GetSensorDistance(Transform sensor)
+    {
+        float maxDistance = 1000;
+        float distance = maxDistance;
+        RaycastHit raycastHit;
+        int trackLayerMask = LayerMask.GetMask("Track");
+
+        if (Physics.Raycast(sensor.position, sensor.forward, out raycastHit, maxDistance, trackLayerMask))
+        {
+            distance = raycastHit.distance;
+        }
+        Debug.DrawRay(sensor.position, sensor.forward * distance, Color.green);
+        return distance;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCarState();
+    }
+}
